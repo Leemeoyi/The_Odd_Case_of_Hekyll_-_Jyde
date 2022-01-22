86deabcdae11d4cbf0e39866d8ae0bae11a7e7b3
JoneLeong <98171510+JoneLeong@users.noreply.github.com>
2022-01-22 22:31:09 +0800

    add stop method in Timer.cs

--- Assets/0_Project/1_Scripts/UI/Timer.cs
+++ Assets/0_Project/1_Scripts/UI/Timer.cs
@@ -5,30 +5,33 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Text))]
-public class Timer : MonoBehaviour
-{
+public class Timer : MonoBehaviour {
     Text timer;
+    bool isStop = false;
 
     private void Awake() {
         timer = GetComponent<Text>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        CoreManager.playTime += Time.deltaTime;
-        timer.text = timeFormater(CoreManager.playTime);
+    
+    void Update() {
+        if (!isStop) {
+            CoreManager.playTime += Time.deltaTime;
+            timer.text = timeFormater(CoreManager.playTime);
+        }
     }
 
     string timeFormater(float currentTime) {
         int hours = TimeSpan.FromSeconds(currentTime).Hours;
         int minutes = TimeSpan.FromSeconds(currentTime).Minutes;
         int secs = TimeSpan.FromSeconds(currentTime).Seconds;
-
         string format = hours > 0
             ? "{0:00}:{1:00}:{2:00}"
             : "{1:00}:{2:00}";
             
         return string.Format(format, hours, minutes, secs);
+    }
+
+    public void stop() {
+        isStop = true;
     }
 }
