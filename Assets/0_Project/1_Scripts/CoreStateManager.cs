601b9af83a908580d703bac41284fe3766085ab5
JoneLeong <98171510+JoneLeong@users.noreply.github.com>
2022-01-23 20:39:39 +0800

    Update CoreStateManager to restrict police and folk count from 0 to positive infinity

--- Assets/0_Project/1_Scripts/CoreStateManager.cs
+++ Assets/0_Project/1_Scripts/CoreStateManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoreStateManager : MonoBehaviour {   
-    [Range(0,  100)] public int police = 10, folk = 30;
+    [Min(0)] [Tooltip("start from 0")]
+    public int police = 10, folk = 30;
 
     [Header("Skill")]
     public int skillCount = 3;
