a68d5038f6b7ad8470b42dc53d83b3e900a44a26
YooLo-maker <74701224+qiuji10@users.noreply.github.com>
2022-01-24 21:41:32 +0800

    Adding Player Core and modify Player Ability and Controller

--- Assets/0_Project/1_Scripts/Player/PlayerController.cs
+++ Assets/0_Project/1_Scripts/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerCore))]
 public class PlayerController : MonoBehaviour
 {
     //add rigidbody2d component to the player
@@ -9,8 +10,13 @@ public class PlayerController : MonoBehaviour
     public float speed;
 
     public Rigidbody2D rb;
-
+    PlayerCore playerCore;
     Vector2 movement;
+
+    void Awake()
+    {
+        playerCore = GetComponent<PlayerCore>();
+    }
 
     void Update()
     {
